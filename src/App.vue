14e5e51b23c0accf867da13c7ae0ba97004ab62f
Jacob Rennemark <jacobrane.se@gmail.com>
2025-02-08 16:48:31 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<script>
+
+
+</script>
+
+<template>
+
+  <header>
+    <nav id="nav-bar">
+            <ul>
+                <li>
+                  <RouterLink to="/">Library</RouterLink>
+                </li>
+                <li>
+                  <RouterLink to="/search">Search</RouterLink>
+                </li>
+            </ul>
+        </nav>
+  </header>
+
+  <main>
+    <router-view>
+    </router-view>
+  </main>
+  
+
+</template>
+
+<style>
+
+header {
+    background-color: #121212;
+    padding: 1rem 2rem;
+    position: fixed;
+    width: 100%;
+    top: 0;
+    left: 0;
+    z-index: 1000;
+}
+
+#nav-bar {
+    display: flex;
+    justify-content: center;
+}
+
+#nav-bar ul {
+    list-style: none;
+    display: flex;
+    gap: 2rem;
+    padding: 0;
+    margin: 0;
+}
+
+#nav-bar li {
+    display: inline;
+}
+
+#nav-bar a {
+    text-decoration: none;
+    color: white;
+    font-size: 1.5rem;
+    font-weight: bold;
+    padding: 10px 15px;
+}
+
+#nav-bar a:hover {
+    background-color: #333;
+    color: #770053; 
+}
+
+
+</style>
---
+++ src/router.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+import Library from './views/Library.vue'
+import Search from './views/Search.vue'
+
+
+export default createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    {
+      component: Library,
+      path: '/'
+    },
+    {
+        component: Search,
+        path: '/Search'
+    },
+  ]
+})
